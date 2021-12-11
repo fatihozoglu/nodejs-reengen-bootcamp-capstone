b06b9bd82840473a26256cc24bc49e0f94ea9656
Fatih <68196068+fatihozoglu@users.noreply.github.com>
2021-12-11 15:41:48 +0300

    feat: install dependencies and create modular structure

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+require("./db_config/index").connect();
+
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server started listening at ${port}.`);
+});
