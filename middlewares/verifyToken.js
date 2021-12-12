2d0ea45e51652ef230bef79834f599c5e1a0dae3
Fatih <68196068+fatihozoglu@users.noreply.github.com>
2021-12-12 21:13:56 +0300

    feat: add verifyToken.js middleware to verify incoming jwt tokens

---
+++ middlewares/verifyToken.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  const token = req.headers["x-access-token"];
+
+  if (!token) {
+    res.status(403).send("You don't have authorization to see this page");
+  } else {
+    try {
+      const verifiedToken = jwt.verify(token, process.env.TOKEN_KEY);
+      req.user = verifiedToken;
+    } catch (err) {
+      res
+        .status(401)
+        .send(
+          "You don't have authorization to see this page. Please Sign in or Register"
+        );
+    }
+  }
+  next();
+};
+
+module.exports = verifyToken;
